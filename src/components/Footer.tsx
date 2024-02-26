da0bf95a56240be93f459c412e3481946f653ade
Quy Leu Duc <quy.leu@kyanon.digital>
2024-02-26 16:05:34 +0700

    modify main

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: FC = () => (
         to="https://alim1496.github.io/"
         className="hover:underline hover:font-bold opacity-85 hover:opacity-100"
       >
-        M A Alim
+       Lều Đức Quý
       </Link>
     </div>
   </div>
